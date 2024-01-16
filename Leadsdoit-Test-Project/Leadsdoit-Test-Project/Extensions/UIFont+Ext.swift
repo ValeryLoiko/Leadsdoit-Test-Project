b35cb79db3eec9707217844b0702dd5ee75e17d9
Valery Loiko <valery.loiko.ios@gmail.com>
2024-01-16 23:28:31 +0100

    add Font, add SnapKit
    check in fonts

---
+++ Leadsdoit-Test-Project/Leadsdoit-Test-Project/Extensions/UIFont+Ext.swift
@@ -0,0 +1,17 @@
+//
+//  UIFont+Ext.swift
+//  Leadsdoit-Test-Project
+//
+//  Created by Diana on 16/01/2024.
+//
+
+import Foundation
+import UIKit
+
+extension UIFont {
+    static let largeTitle = UIFont(name: "SFPro-Bold", size: 34)
+    static let title2 = UIFont(name: "SFPro-Bold", size: 22)
+    static let title = UIFont(name: "SFPro-Regular", size: 22)
+    static let body2 = UIFont(name: "SFPro-Bold", size: 17)
+    static let body = UIFont(name: "SFPro-Regular", size: 16)
+}
