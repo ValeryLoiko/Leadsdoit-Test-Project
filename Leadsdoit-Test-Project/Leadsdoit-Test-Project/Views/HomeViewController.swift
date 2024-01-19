69c5713d9534573507ff76fff5179b992ba98951
Valery Loiko <valery.loiko.ios@gmail.com>
2024-01-19 11:33:53 +0100

    create views for HomeVC
    create orange view with all icons
    add tableView
    add all outlets for views

---
+++ Leadsdoit-Test-Project/Leadsdoit-Test-Project/Views/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Leadsdoit-Test-Project
+//
+//  Created by Diana on 16/01/2024.
+//
+
+import UIKit
+import SnapKit
+
+class HomeViewController: UIViewController {
+
+    //MARK: - Outlets
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var filterCamera: UIView!
+    @IBOutlet weak var roverCamera: UIView!
+    @IBOutlet weak var calendar: UIImageView!
+    @IBOutlet weak var add: UIView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+
+
+}
+
